da180deebcb3a6512b28c2f20ca56901499b961d
simonstar@wp.pl <szymon@DELL.(none)>
2012-03-11 17:02:31 +0100

    Move controler changes + Main class

--- src/nxt/rurek/MoveControler.java
+++ src/nxt/rurek/MoveControler.java
@@ -1,17 +1,16 @@
 package nxt.rurek;
 
+import lejos.nxt.Motor;
 import lejos.nxt.NXTRegulatedMotor;
 import lejos.nxt.TachoMotorPort;
 
 
 public class MoveControler {
-	NXTRegulatedMotor left;
-	NXTRegulatedMotor right;
+	NXTRegulatedMotor left = Motor.A;
+	NXTRegulatedMotor right = Motor.B;
 	private static int max_speed = 100;
 	
-	public MoveControler(TachoMotorPort left, TachoMotorPort right) {
-		this.left = new NXTRegulatedMotor(left);
-		this.right = new NXTRegulatedMotor(right);
+	public MoveControler() {
 	}
 	
 	public void goToPosition(Direction direction) {
---
+++ src/nxt/rurek/Rurek.java
@@ -0,0 +1,16 @@
+package nxt.rurek;
+
+import lejos.nxt.Button;
+
+public class Rurek {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		while (!Button.ESCAPE.isDown()) {
+			
+		}
+	}
+
+}
