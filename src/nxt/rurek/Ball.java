3bb7808e9225e063fd2ac7558c192224e1ca14b1
Tomasz Pawłowski <tptpawlowski@gmail.com>
2012-03-11 16:24:02 +0100

    Ball

---
+++ src/nxt/rurek/Ball.java
@@ -0,0 +1,49 @@
+package nxt.rurek;
+
+import lejos.nxt.SensorPort;
+import lejos.nxt.addon.CompassHTSensor;
+import lejos.nxt.addon.IRSeekerV2;
+
+public class Ball {
+
+	
+	private IRSeekerV2 ir;
+	
+	public Ball () {
+		super();
+		ir = new IRSeekerV2(SensorPort.S2, IRSeekerV2.Mode.AC);
+	}
+	
+	private double getMean (int a, int b) {
+		return ((double)b)/(a+b);  
+	}
+	
+	/**
+	 * Function findBall
+	 * @return angle in [-180, 180] saying where is the ball.
+	 */
+	public double findBall() {
+		int[] val = ir.getSensorValues();
+		
+		int sum = 0;
+		for (int i : val) {
+			sum += i;
+		}
+		if (sum < 100) {
+			return -1000;
+		}
+		
+		int m = 0;
+		int best = 0;
+		for (int i = 0; i < 5; ++i) {
+			if (m < val[i%5] + val[(i+1)%5]) {
+				m = val[i%5] + val[(i+1)%5];
+				best = i;
+			}
+		}
+		
+		double mul = (best < 4) ? 60.0 : 120.0;
+		
+		return 60*(best-2)+ mul*getMean(val[best], val[(best+1)%5]);
+	}
+}
