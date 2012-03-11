8b63676016905c7da2f6faf68114a01687422e09
Tomasz Pawłowski <tptpawlowski@gmail.com>
2012-03-11 16:16:38 +0100

    Direction

---
+++ src/nxt/rurek/Direction.java
@@ -0,0 +1,23 @@
+package nxt.rurek;
+
+public class Direction {
+	private float angle, distance;
+
+	public float getAngle() {
+		return angle;
+	}
+
+	public void setAngle(float angle) {
+		this.angle = angle;
+	}
+
+	public float getDistance() {
+		return distance;
+	}
+
+	public void setDistance(float distance) {
+		this.distance = distance;
+	}
+	
+	
+}
