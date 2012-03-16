417506874797f5f08a77a9dba81af593f9a4c05f
Szymon Starzycki <szymon@DELL.(none)>
2012-03-16 13:30:11 +0100

    PositionController with compass pilot

--- src/nxt/rurek/PositionController.java
+++ src/nxt/rurek/PositionController.java
@@ -1,32 +1,39 @@
 package nxt.rurek;
 
 import lejos.nxt.Motor;
-import lejos.robotics.navigation.DifferentialPilot;
+import lejos.nxt.SensorPort;
+import lejos.nxt.addon.CompassHTSensor;
+import lejos.robotics.navigation.CompassPilot;
+//import lejos.robotics.navigation.DifferentialPilot;
 
+@SuppressWarnings("deprecation")
 public class PositionController {
 	private static int max_speed = 360;
 	private static int max_round_speed = 150; 
-	private DifferentialPilot plot;
+	private CompassPilot pilot;
+	
 	
 	public PositionController() {
-		plot = new DifferentialPilot(8.95, 14.8, Motor.B, Motor.A);
-		plot.setRotateSpeed(4);
+		//pilot = new DifferentialPilot(8.95, 14.8, Motor.B, Motor.A);
+		pilot = new CompassPilot(new CompassHTSensor(SensorPort.S4), 8.95f, 14.8f, Motor.B, Motor.A);
+		pilot.setTravelSpeed(80);
 	}
 	
-	DifferentialPilot getDifferentialPilot() {
-		return plot;
+	CompassPilot getDifferentialPilot() {
+		return pilot;
 	}
 
 	public void goToPosition(Direction direction) {
 		if(!direction.isInRange()){
-			plot.rotateLeft();
+			pilot.rotateLeft();
 		}
 		else {
 			if(Math.abs(direction.getAngle()) < 30) {
-				plot.forward();
+				pilot.forward();
 			}
 			else {
-				plot.arcForward(Math.sin(direction.getAngle()) * (120 - Math.abs(direction.getAngle())));
+				//pilot.arcForward(Math.signum(direction.getAngle()) * -1 * (125 - Math.abs(direction.getAngle())) / 2);
+				pilot.steer(direction.getAngle() * -1);
 			}
 		}
 	}
